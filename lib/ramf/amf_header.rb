3d8cbec67cf0849e2e69ba45b3105ef5b06d549e
Neer Friedman <neer@neer-laptop.(none)>
2008-08-25 08:49:57 +0300

    added length attribute to AmfMessage and AmfHeader for future use

--- lib/ramf/amf_header.rb
+++ lib/ramf/amf_header.rb
@@ -1,9 +1,9 @@
 module RAMF
   class AMFHeader
-    attr_accessor :name, :value, :must_understand
+    attr_accessor :name, :value, :must_understand, :length
     
-    def initialize(name,value,must_understand)
-      @name, @value, @must_understand = name, value, must_understand
+    def initialize(name,value,must_understand, length = -1)
+      @name, @value, @must_understand, @length = name, value, must_understand, length
     end
   end
 end
--- lib/ramf/amf_message.rb
+++ lib/ramf/amf_message.rb
@@ -1,11 +1,12 @@
 module RAMF
   class AMFMessage
-    attr_accessor :target_uri, :response_uri, :value
+    attr_accessor :target_uri, :response_uri, :value, :length
     
     def initialize(options)
       @target_uri = options[:target_uri]
       @response_uri = options[:response_uri]
       @value = options[:value]
+      @length = options[:length] || -1
     end
     
   end
