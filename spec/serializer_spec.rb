418dd23e3b457d3ca4322889fc39bd430a374347
Neer Friedman <neer@neer-laptop.(none)>
2008-08-25 08:57:13 +0300

    added initial serializer spec

---
+++ spec/serializer_spec.rb
@@ -0,0 +1,24 @@
+require File.join(File.dirname(__FILE__),'spec_helper')
+
+describe RAMF::Deserializer::Base do
+  
+  before(:all) do
+    @amfobject = RAMF::AMFObject.new
+    @serializer = RAMF::Serializer::Base.new(3)
+  end
+    
+  it 'should work' do
+    value = RAMF::FlexObjects::FlexAnonymousObject.new
+    value.attr1 = "1abc"
+    value.attr2 = "false"
+    @amfobject.add_message new_amf_message(value)
+    #TODO: check that the message is good
+  end
+  
+end
+
+def new_amf_message(value)
+  RAMF::AMFMessage.new(:target_uri=>"1\onResult",
+                       :response_uri=>"null",
+                       :value=>value)
+end
