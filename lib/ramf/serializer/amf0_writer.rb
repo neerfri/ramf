5781f7ce68ffab1c35376075ab3fee5cec79de4b
Neer Friedman <neer@neer-laptop.(none)>
2008-09-05 10:42:31 +0300

    Ahh, I think you'll notice the changes (:

--- lib/ramf/serializer/amf0_writer.rb
+++ lib/ramf/serializer/amf0_writer.rb
@@ -14,8 +14,58 @@ module RAMF
       def write_value_type(object,stream)
         case
           when object.nil?
-        end    
+            stream << AMF0_NULL_MARKER
+          when object.is_a?(Numeric)
+            stream << AMF0_NUMBER_MARKER
+            write_double(object,stream)
+          when object.is_a?(String) || object.is_a?(Symbol)
+            if object.to_s.length <= 65535
+              stream << AMF0_STRING_MARKER
+              writeUTF8(object, stream)
+            else
+              stream << AMF0_LONG_STRING_MARKER
+              writeUTF8Long(object, stream)
+            end
+          when object.is_a?(TrueClass) || object.is_a?(FalseClass)
+            stream << AMF0_BOOLEAN_MARKER
+            writeU8(object ? 1:0, stream)
+          when object.is_a?(Array)
+            stream << AMF0_ARRAY_MARKER
+            write_strict_array(object, stream)
+          when object.is_a?(Hash)
+            stream << AMF0_ECMA_ARRAY_MARKER 
+            write_ecma_array(object, stream)
+          when object.is_a?(Date) || object.is_a?(Time)
+            stream << AMF0_DATE_MARKER
+            write_date(object, stream)
+          when object.class.name == "REXML::Document"
+            stream << AMF0_XML_MARKER
+            write_xml(object, stream)  
+        end
       end
-    end
+      
+      def write_strict_array(array, stream)
+        writeU32(array.size, stream)
+        array.each {|v| write_value_type(v, stream)}
+      end
+      
+      def write_ecma_array(hash, stream)
+        writeU32(hash.size, stream)
+        hash.each {|k,v| writeUTF8(k.to_s); write_value_type(v)}
+        writeUTF8("")
+        stream << AMF0_OBJECT_END_MARKER
+      end
+      
+      def write_date(date, stream)
+        secs = date.is_a?(Time) ? date.utc.to_i : date.strftime("%s").to_i
+        write_double(secs.to_i * 1000,stream)
+        steram << "\000\000" #reserved for timezone serialization, not used.
+      end
+    
+      def write_xml(xml, stream)
+        writeUTF8Long(xml.to_s, stream)
+      end
+    
+    end      
   end
 end
