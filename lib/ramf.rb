2c050fa8ee05153b7890f3705fb8a646712053e0
Neer Friedman <neer@neer-laptop.(none)>
2008-10-24 09:54:15 +0200

    removed traces of debug mode

--- lib/ramf.rb
+++ lib/ramf.rb
@@ -1,62 +1,46 @@
 
 $:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-require 'stringio'
-module RAMF; end;
-  
-#Load configuration class
-require 'ramf/util'
-require 'ramf/configuration'
-
-
-#Extensions to existing Ruby classes.
-require 'ramf/flex_class_traits'
-require 'ramf/extensions/class'
-require 'ramf/extensions/object'
-require 'ramf/extensions/hash'
-require 'ramf/extensions/exception'
-
-#RAMF fundemental objects
-require 'ramf/amf_object'
-require 'ramf/amf_header'
-require 'ramf/amf_message'
-require 'ramf/operation_request'
-require 'ramf/operation_processors_manager'
-require 'ramf/default_operation_processor'
-
-require 'ramf/io/flex_class_signature'
-require 'ramf/flex_objects/flex_anonymous_object'
-require 'ramf/flex_objects/flex_object'
-require 'ramf/flex_objects/acknowledge_message'
-require 'ramf/flex_objects/error_message'
-require 'ramf/flex_objects/remoting_message'
-require 'ramf/flex_objects/command_message'
-require 'ramf/flex_objects/byte_array'
-
-
-require 'ramf/io/constants'
-require 'ramf/io/common_read_write'
-require 'ramf/io/place_holder'
-require 'ramf/io/reference_table'
-require 'ramf/deserializer'
-require 'ramf/serializer'
-
 module RAMF
-  class NullLogger
-    def debug(message); end;
-  end
   
-  def self.define_debug_logger
-    begin
-      puts "initializing RAMF debug log file"
-      require 'fileutils'
-      require 'logger'
-      log_path = File.join(File.dirname(__FILE__),'../debug/debug.log')
-      FileUtils.mkdir_p File.dirname(log_path)
-      Logger.new(log_path)
-    rescue Exception=>e
-      NullLogger.new()
-    end
-  end
+  #load requirements from ruby
+  require 'stringio'
+
+  #Load configuration class
+  require 'ramf/util'
+  require 'ramf/configuration'
   
-  DEBUG_LOG =  defined?(RAMF_DEBUG) || ENV["RAMF_DEBUG"] ? define_debug_logger : NullLogger.new()
+  
+  #Extensions to existing Ruby classes.
+  require 'ramf/flex_class_traits'
+  require 'ramf/extensions/class'
+  require 'ramf/extensions/object'
+  require 'ramf/extensions/hash'
+  require 'ramf/extensions/exception'
+  
+  #RAMF fundemental objects
+  require 'ramf/amf_object'
+  require 'ramf/amf_header'
+  require 'ramf/amf_message'
+  require 'ramf/operation_request'
+  require 'ramf/operation_processors_manager'
+  require 'ramf/default_operation_processor'
+  
+  require 'ramf/io/flex_class_signature'
+  require 'ramf/flex_objects/flex_anonymous_object'
+  require 'ramf/flex_objects/flex_object'
+  require 'ramf/flex_objects/acknowledge_message'
+  require 'ramf/flex_objects/error_message'
+  require 'ramf/flex_objects/remoting_message'
+  require 'ramf/flex_objects/command_message'
+  require 'ramf/flex_objects/byte_array'
+  
+  
+  require 'ramf/io/constants'
+  require 'ramf/io/common_read_write'
+  require 'ramf/io/place_holder'
+  require 'ramf/io/reference_table'
+  require 'ramf/deserializer'
+  require 'ramf/serializer'
+
 end
+  
