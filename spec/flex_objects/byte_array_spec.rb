1e0664997b9951dcdafefda5ac3fc87654f7bda2
Neer Friedman <neer@neer-laptop.(none)>
2008-11-13 17:32:40 +0200

    fix example in byte_array_spec

--- spec/flex_objects/byte_array_spec.rb
+++ spec/flex_objects/byte_array_spec.rb
@@ -5,7 +5,7 @@ describe RAMF::FlexObjects::ByteArray do
   before(:each) {@byte_array = RAMF::FlexObjects::ByteArray.new("content")}
   
   it "should be kind_of IO" do
-    @byte_array.should be_kind_of(::Tempfile)
+    @byte_array.kind_of?(::Tempfile).should be_true
   end
   
   it "should be rewinded" do
